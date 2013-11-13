b3d155010e395f63c5404ac63585485cb947cad5
Eivind Sandstad <eivind.sandstad@gmail.com>
2013-11-13 13:18:42 +0100

    Fix to match the scheduler

--- src/no/ntnu/item/ttm4160/sunspot/runtime/IStateMachine.java
+++ src/no/ntnu/item/ttm4160/sunspot/runtime/IStateMachine.java
@@ -2,10 +2,8 @@ package no.ntnu.item.ttm4160.sunspot.runtime;
 
 
 public interface IStateMachine {
-    public static final int
-            EXECUTE_TRANSITION = 0,
-            DISCARD_EVENT = 1,
-            TERMINATE_SYSTEM = 2;
 
-    public int fire(Event event, Scheduler scheduler);
+
+    public EAction fire(Event event, Scheduler scheduler);
+    public boolean answersTo(String id);
 }
