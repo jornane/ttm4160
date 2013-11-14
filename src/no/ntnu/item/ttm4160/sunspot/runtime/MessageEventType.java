401c4431d276eb97f910150aee2f2025601aeb43
yorn <git@yorn.priv.no>
2013-11-14 14:07:20 +0100

    Removed support for messageKind in MessageEventType

--- src/no/ntnu/item/ttm4160/sunspot/runtime/MessageEventType.java
+++ src/no/ntnu/item/ttm4160/sunspot/runtime/MessageEventType.java
@@ -8,8 +8,6 @@ public class MessageEventType implements IEventType {
 	
 	public final String receiver;
 
-	private String messageKind;
-
 	/**
 	 * Construct a new MessageEvent Type.
 	 * It will match on a receiver.
@@ -22,25 +20,9 @@ public class MessageEventType implements IEventType {
 		this.receiver = receiver;
 	}
 
-	/**
-	 * Construct a new MessageEvent Type.
-	 * It will match on a receiver and a kind of message.
-	 * 
-	 * @param receiver	the receiver
-	 */
-	public MessageEventType(String receiver, String messageKind) {
-		if (receiver == null)
-			throw new NullPointerException();
-		if (messageKind == null)
-			throw new NullPointerException();
-		this.receiver = receiver;
-		this.messageKind = messageKind;
-	}
-
 	public boolean isInterestedIn(Event event) {
 		return event instanceof MessageEvent
 				&& receiver.equals(((MessageEvent)event).message.getReceiver())
-				&& ((MessageEvent)event).message.getContent().startsWith(messageKind)
 				;
 	}
 
